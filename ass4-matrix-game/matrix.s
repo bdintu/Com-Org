2bbd6088bdbb53560a2d76db5d3cbb90cd1f2512
j <j@j.j>
2018-03-29 21:43:30 +0700

    bug crash bullet

--- ass4-matrix-game/matrix.s
+++ ass4-matrix-game/matrix.s
@@ -3,6 +3,7 @@ X_MAX			equ		25 + RAIN_SIZE
 DEFAULT_RAIN	equ		-01h
 DEFAULT_BULLET	equ		21
 LIFE_POS		equ		2*(1*80 + 9)
+COL_N			equ		0ah
 
 .model	tiny
 
@@ -10,10 +11,10 @@ LIFE_POS		equ		2*(1*80 + 9)
 	seed	dw	?
 	life	db	'9', 0fh
 
-	rain_y	db	10	dup(DEFAULT_RAIN)
-	rain_x	db	14, 20, 26, 32, 38, 44, 50, 56, 62, 68
+	rain	db	10	dup(DEFAULT_RAIN)
+	lane	db	14, 20, 26, 32, 38, 44, 50, 56, 62, 68
 
-	bullet_y	db	10 dup(DEFAULT_BULLET)
+	bullet  db	10 DUP(DEFAULT_BULLET)
 
 	; ''.join([chr(i) for i in range(33, 127)])
 	alpha	db	'!#$%&\()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}'	; 90
@@ -118,39 +119,212 @@ main:
 while1:	
 	call	delay
 
-	call	getch
-	cmp		al,		1Bh						; isEsc
-	je		exit_half
+	;mov		ah,		01h
+	;int		16h
+	;cmp		al,		1Bh						; isEsc
+	;je		exit_half
 
 	call	rand_rainpos					; ret seed
 	mov		si,			seed
-	cmp		rain_y[si],	DEFAULT_RAIN
+	cmp		rain[si],	DEFAULT_RAIN
 	jne		rander_init
-	mov		rain_y[si],	DEFAULT_RAIN + 1
+	mov		rain[si],	DEFAULT_RAIN + 1
 	
 	rander_init:
 		mov		si,		00h
 	rander_test:
-		cmp		si,		0ah
-		je		while1
+		cmp		si,		COL_N
+		je		main_bullet
 	rander_body:
-		cmp		rain_y[si],	DEFAULT_RAIN
+		cmp		rain[si],	DEFAULT_RAIN
 		je		rander_inc
 
+		call	rand_char				; ret char to al
 		call	print_rain
+
 		call	chk_life
 		call	print_life	
 
-		inc		rain_y[si]
+		inc		rain[si]
 	
-		cmp		rain_y[si],	X_MAX
+		cmp		rain[si],	X_MAX
 		jne		rander_inc
-		mov		rain_y[si],	DEFAULT_RAIN
+		mov		rain[si],	DEFAULT_RAIN
 
 	rander_inc:
 		inc		si
 		jmp		rander_test
-	ret
+		
+	main_bullet:
+		mov     ah, 01h
+		int     16h
+		jne     getch
+
+		xor     si, si
+		call    drawbullet
+
+	crash_init:
+		mov		si,		00h
+	crash_test:
+		cmp		si,		COL_N
+		je		goto_while1
+	crash_body:
+		mov		dh,		bullet[si]
+		mov		dl,		rain[si]
+		sub		dh,		dl				; bullet = bullet - rain
+		cmp		dh,		0h
+		jne		crash_inc
+
+		mov		al,		' '				; ret char to al
+		call	print_rain
+
+		dec		life
+		call	chk_life
+		call	print_life	
+
+		mov		rain[si],	DEFAULT_RAIN
+		mov		bullet[si], DEFAULT_BULLET
+	crash_inc:
+		inc		si
+		jmp		crash_test		 
+goto_while1:
+	jmp		while1
+
+getch:
+	mov     ah, 00h
+	int     16h
+	call    createBullet
+	cmp     bullet[si], DEFAULT_BULLET
+	jne     @A
+	mov     bullet[si], DEFAULT_BULLET -1
+	@A:
+	jmp     main_bullet
+
+incsi:
+inc     si
+drawbullet:
+    cmp     si, 10
+    je      enddrawbullet
+    cmp     bullet[si], DeFAULT_BULLET
+    je      incsi
+
+    dec 	bullet[si]
+    ;draw bullet
+    ;-------------------
+    ; set position di
+    ;-------------------
+    push 	ax
+    push 	dx
+    mov 	ax, 80
+    mov 	dl, bullet[si]
+    mul 	dl
+    add 	ax, 14
+
+    push    ax
+    mov     ax, 6
+    mul     si
+    mov     dx, ax
+    pop     ax
+    add     ax, dx
+    
+    mov 	dx, 2
+    mul 	dx
+    mov 	di, ax
+    pop 	dx
+    pop 	ax
+    ;-------------------
+    ; draw bullet
+    ;-------------------
+    mov     al, 'A'
+    mov 	ah, 0fh
+    mov 	cx, 1
+    rep 	stosw
+    ;-------------------
+    ; delete bullet
+    ;-------------------
+    add     di, 158
+    mov     al, ' '
+    mov 	ah, 0fh
+    mov 	cx, 1
+    rep 	stosw
+    
+    cmp     bullet[si], 0
+    je      @b
+    jmp     incsi
+    @b:
+    push    ax
+    mov     ax, si
+    mov     dx, 6
+    mul     dx
+    add     ax, 14
+    mov     dx, 2
+    mul     dx
+    mov     di, ax
+    pop     ax
+    mov     al, ' '
+    mov 	ah, 0fh
+    mov 	cx, 1
+    rep 	stosw
+
+    mov     bullet[si], 21
+    jmp     drawbullet
+
+    enddrawbullet:
+ret
+
+createBullet:
+    Q:
+    cmp     al, 'q'
+    jne     W
+    mov     si, 0
+    ret
+    W:
+    cmp     al, 'w'
+    jne     E
+    mov     si, 1
+    ret
+    E:
+    cmp     al, 'e'
+    jne     R
+    mov     si, 2
+    ret
+    R:
+    cmp     al, 'r'
+    jne     T
+    mov     si, 3
+    ret
+    T:
+    cmp     al, 't'
+    jne     Y
+    mov     si, 4
+    ret
+    Y:
+    cmp     al, 'y'
+    jne     U
+    mov     si, 5
+    ret
+    U:
+    cmp     al, 'u'
+    jne      I
+    mov     si, 6
+    ret
+    I:
+    cmp     al, 'i'
+    jne     O
+    mov     si, 7
+    ret
+    O:
+    cmp     al, 'o'
+    jne     P
+    mov     si, 8
+    ret
+    P:
+    cmp     al, 'p'
+    jne     endcreateBullet
+    mov     si, 9
+
+    endcreateBullet:
+ret
 
 exit_half:
 	jmp		exit
@@ -178,14 +352,14 @@ clrscr:
 	mov		dx,		0
 	call	print
 
-getch:
-	mov		ah,		01h
-	int		16h
-	ret
+;getch:
+;	mov		ah,		01h
+;	int		16h
+;	ret
 
 delay:
 	mov     cx,		01h
-	mov     dx,		0ffffh
+	mov     dx,		0fffh
 	mov     ah,		86h
 	int     15H
 	ret
@@ -225,7 +399,7 @@ printxy:								; get ax, dx
 	ret
 
 chk_life:
-	cmp		rain_y[si],	DEFAULT_BULLET
+	cmp		rain[si],	DEFAULT_BULLET
 	jne		exit_chk_life
 	dec		life
 	exit_chk_life:
@@ -238,10 +412,8 @@ print_life:
 	ret
 
 print_rain:
-	mov		dl,		rain_x[si]
-	mov		dh,		rain_y[si]
-
-	call	rand_char				; ret char to al
+	mov		dl,		lane[si]
+	mov		dh,		rain[si]
 
 	mov		ah, 	0fh				; white
 	call	printxy
